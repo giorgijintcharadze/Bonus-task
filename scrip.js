8eb59a7e20ba21f1248a746c6a534275c5571634
giorgijintcharadze <giorgijintcharadze7@gmail.com>
2025-07-03 18:05:54 +0400

    added bonus task

---
+++ scrip.js
@@ -0,0 +1,20 @@
+let userinput = document.querySelector("#input");
+let button = document.querySelector("#clickbtn");
+let username = document.querySelector("#username");
+let Biography = document.querySelector("#bio");
+let imgOutput = document.querySelector("#Githubpicture");
+
+button.addEventListener("click", () => {
+  document.querySelector("#outputcontainer").classList.add("visable");
+  fetch(`https://api.github.com/users/${userinput.value}`)
+    .then((info) => info.json())
+    .then((data) => {
+      username.textContent = data.login;
+      if (data.bio) {
+        Biography.textContent = data.bio;
+      } else {
+        username.textContent = "error";
+      }
+      imgOutput.src = data.avatar_url;
+    });
+});
